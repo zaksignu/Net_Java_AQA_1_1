dda263873a0976fde480908f32e3d5e71dfe042c
zaksignu <zaksignu@gmail.com>
2022-03-16 14:57:16 +0300

    Minor Changes for tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -8,14 +8,14 @@ public class CashbackHackServiceTest {
     static CashbackHackService cb = new CashbackHackService();
 
     @Test
-    public void shouldNotWorkWell() {
+    public void shouldWorkWellAtEdge() {
         int actual = 0;
         int expected = cb.remain(1000);
         assertEquals(actual, expected);
     }
 
     @Test
-    public void shouldWorkWell2() {
+    public void shouldWorkWell() {
 
         int actual = 700;
         int expected = cb.remain(300);
@@ -23,7 +23,7 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void shouldWorkWell3() {
+    public void shouldWorkWellMoreThan1000() {
 
         int actual = 900;
         int expected = cb.remain(1100);
